fe6c62604615aa374ffb58c7048a3f763a4a0e52
Hometgar <m.redon@outlook.fr>
2017-06-19 17:23:18 +0200

    module user:    modification du constructeur,    cryptage du mot de passe,    verification du mot de passe,    verification des login de l'utilisateur

--- user/__init__.py
+++ user/__init__.py
@@ -1,15 +1,21 @@
-import hashlib
+import hashlib, uuid
 
 class User:
 
-    def __init__(self, conf):
-        user = conf.get('user')
-        self._name = user['name']
-        self._password = user['pass']
+    def __init__(self, username, password):
+        if(not username or not password):
+            raise ValueError('INVALID ENTRY: SHOULD HAVE name AND pass KEY')
+        if type(password) is not str and type(password) is not unicode or type(username) is not str and type(username) is not unicode:
+            raise ValueError('INVALID ENTRY')
+        self._name = username
+        self._password = password
+
+    def __str__(self):
+        return self._name
 
     def _set_name(self, name):
-        if type(name) is not str:
-            raise "INVALID USERNAME"
+        if type(name) is not str and type(name) is not unicode:
+            raise ValueError("INVALID USERNAME")
         else:
             self._name = name
 
@@ -17,18 +23,26 @@ class User:
         return self._name
 
     def _set_password(self, password):
-        if type(password) is not str:
-            raise "INVALID PASSWORD"
+        if type(password) is not str and type(password) is not unicode:
+            raise ValueError("INVALID PASSWORD")
         else:
-            self._password = password
+            if len(password) < 6 :
+                raise ValueError("INVALID PASSWORD: TO SHORT")
+            salt = uuid.uuid4().hex
+            self._password = hashlib.sha256(salt.encode()+password.encode()).hexdigest()+':'+salt.encode()
 
     def _get_password(self):
-        raise "NOT AUTHORISED"
+        raise KeyError("NOT AUTHORISED")
 
-    #TODO redo avec crypto...
+    def verify_password(self, entry):
+        password, salt = self._password.split(':')
+        if password == hashlib.sha256(salt.encode()+ entry.encode()).hexdigest():
+            return True
+        return False
+
+
     def connect(self, username, user_password):
-        password, salt = self.password.split(':')
-        if not (self.name == username) and not(password == hashlib.sha256(salt.encode()+ user_password.encode).hexdigest()):
+        if not (self._name == username) and not self.verify_password(user_password):
             return False
         return True
 
